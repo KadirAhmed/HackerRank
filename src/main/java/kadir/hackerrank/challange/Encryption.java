f2d657f8caea5308f29f78be235703c6e4a8af6e
ahmed <mail.ahmed6@gmail.com>
2023-07-24 01:29:25 +0600

    Solution to the Encryption problem in HackerRank Problem Link:https://www.hackerrank.com/challenges/encryption/problem

---
+++ src/main/java/kadir/hackerrank/challange/Encryption.java
@@ -0,0 +1,63 @@
+package kadir.hackerrank.challange;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Encryption {
+    public static String encryption(String s) {
+    // Write your code here
+    
+        List strings = new ArrayList<String>();
+        int row = (int) Math.sqrt(s.length());
+        int col = row + 1;
+        if (row * row == s.length()) {
+            col = row;
+        } else if (row * col < s.length()) {
+            row += 1;
+        } else {
+        }
+        int r, c;
+
+        for (int i = 0; i < row; i++) {
+            r = i * row;
+            c = r + col;
+
+            if (i > 0) {
+                r = col * i;
+                c = r + col;
+            }
+
+            if (c > s.length()) {
+                c = s.length();
+            }
+            String string = s.substring(r, c);
+            System.err.println(string);
+            strings.add(string);
+        }
+
+        String encyptedMsg = "";
+        String message = "";
+        for (int i = 0; i < col; i++) {
+            message = "";
+
+            for (int j = 0; j < row; j++) {
+                try {
+                    message += strings.get(j).toString().charAt(i);
+                } catch (Exception ex) {
+                    continue;
+                }
+            }
+            message += " ";
+            encyptedMsg += message;
+        }
+     return encyptedMsg;
+    }
+    
+    public static void main(String args[]) {
+    	
+    	
+    	
+    	
+    }
+    
+}
