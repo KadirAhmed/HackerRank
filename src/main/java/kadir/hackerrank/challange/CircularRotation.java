c97b632f91cf903a3eeca8861ea904c353c6c653
ahmed <mail.ahmed6@gmail.com>
2023-07-26 14:56:01 +0600

    Solution to the HackerRank Problem: Circular Array Rotation Problem Link:https://www.hackerrank.com/challenges/circular-array-rotation/problem

---
+++ src/main/java/kadir/hackerrank/challange/CircularRotation.java
@@ -0,0 +1,22 @@
+package kadir.hackerrank.challange;
+
+import java.util.List;
+
+public class CircularRotation {
+
+    public static List<Integer> circularArrayRotation(List<Integer> a, int k, List<Integer> queries) {
+        // Write your code here
+        int position;
+        int count = 0;
+        int  rotation = k >= a.size() ? k % a.size() : k;
+
+        for(int x: queries){
+            position =  (x - rotation );
+            position = position <0 ? position + a.size() :  position ;
+            queries.set(count, a.get(position));
+            count++;
+        }
+
+        return queries;
+    }
+}
