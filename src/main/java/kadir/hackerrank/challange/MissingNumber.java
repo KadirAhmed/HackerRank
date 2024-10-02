8348bac4584222d9bc46ffbea8124b49a588b583
ahmed <mail.ahmed6@gmail.com>
2024-10-03 03:16:08 +1000

    link: https://www.hackerrank.com/challenges/missing-numbers/problem

--- src/main/java/kadir/hackerrank/challange/MissingNumber.java
+++ src/main/java/kadir/hackerrank/challange/MissingNumber.java
@@ -8,7 +8,7 @@ import java.util.stream.*;
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
-class Result {
+class MissingNumber {
 
     /*
      * Complete the 'missingNumbers' function below.
@@ -48,38 +48,5 @@ class Result {
             return i;
         }
         return 0;
-    }
-
-
-}
-
-public class MissingNumber {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-
-        int n = Integer.parseInt(bufferedReader.readLine().trim());
-
-        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
-                .map(Integer::parseInt)
-                .collect(toList());
-
-        int m = Integer.parseInt(bufferedReader.readLine().trim());
-
-        List<Integer> brr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
-                .map(Integer::parseInt)
-                .collect(toList());
-
-        List<Integer> result = Result.missingNumbers(arr, brr);
-
-        bufferedWriter.write(
-                result.stream()
-                        .map(Object::toString)
-                        .collect(joining(" "))
-                        + "\n"
-        );
-
-        bufferedReader.close();
-        bufferedWriter.close();
     }
 }
