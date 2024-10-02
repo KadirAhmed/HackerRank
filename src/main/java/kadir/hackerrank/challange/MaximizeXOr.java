99d9c77547c876ef412a87903159e9093ba44146
ahmed <mail.ahmed6@gmail.com>
2024-10-03 04:13:50 +1000

    https://www.hackerrank.com/challenges/maximizing-xor/problem

--- src/main/java/kadir/hackerrank/challange/MaximizeXOr.java
+++ src/main/java/kadir/hackerrank/challange/MaximizeXOr.java
@@ -1,18 +1,8 @@
 package kadir.hackerrank.challange;
 
-import java.io.*;
-        import java.math.*;
-        import java.security.*;
-        import java.text.*;
-        import java.util.*;
-        import java.util.concurrent.*;
-        import java.util.function.*;
-        import java.util.regex.*;
-        import java.util.stream.*;
-        import static java.util.stream.Collectors.joining;
-import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.joining;
 
- public class MaximizeOr {
+public class MaximizeXOr {
 
     /*
      * Complete the 'maximizingXor' function below.
