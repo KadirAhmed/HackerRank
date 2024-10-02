8b66c17382c6e83b6f1704553227364d6b5e52f2
ahmed <mail.ahmed6@gmail.com>
2024-10-03 04:06:08 +1000

    https://www.hackerrank.com/challenges/flipping-bits/problem

---
+++ src/main/java/kadir/hackerrank/challange/FlippingBits.java
@@ -0,0 +1,16 @@
+package kadir.hackerrank.challange;
+
+public class  FlippingBits {
+
+    /*
+     * Complete the 'flippingBits' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts LONG_INTEGER n as parameter.
+     */
+
+    public static long flippingBits(long n) {
+        // Write your code here
+        return n ^ 0xFFFFFFFFL;
+    }
+}
