129446cee8ba3e5095aa200375e64eb1d2ff3441
ahmed <mail.ahmed6@gmail.com>
2024-10-03 20:14:30 +1000

    link: https://www.hackerrank.com/challenges/strange-advertising/problem

--- src/main/java/kadir/hackerrank/challange/ViralAdvertising.java
+++ src/main/java/kadir/hackerrank/challange/ViralAdvertising.java
@@ -1,14 +1,12 @@
 package kadir.hackerrank.challange;
 
 public class ViralAdvertising  {
-
     /*
      * Complete the 'viralAdvertising' function below.
      *
      * The function is expected to return an INTEGER.
      * The function accepts INTEGER n as parameter.
      */
-
     public static int viralAdvertising(int n) {
         // Write your code here
         double recepients = 5;
