1df9182f43d1ad4c13751e9472b228d034601843
ahmed <mail.ahmed6@gmail.com>
2024-10-03 20:01:56 +1000

    link: https://www.hackerrank.com/challenges/compare-the-triplets/problem

--- src/main/java/kadir/hackerrank/challange/CompareTriplets.java
+++ src/main/java/kadir/hackerrank/challange/CompareTriplets.java
@@ -8,8 +8,7 @@ public class CompareTriplets {
     // Complete the compareTriplets function below.
     static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
         int aa = 0, bb = 0;
-        for(int i=0 ; i<3; i++)
-        {
+        for(int i=0 ; i<3; i++) {
             aa+= (int)a.get(i) > (int)b.get(i) ? 1 : 0 ;
             bb+=  (int)a.get(i) < (int)b.get(i) ? 1 : 0 ;
         }
@@ -18,6 +17,5 @@ public class CompareTriplets {
         list.add(bb);
 
         return  list;
-
     }
 }
