0b8a3c745bd4f46767da4592cd276711757117bd
ahmed <mail.ahmed6@gmail.com>
2024-10-03 16:15:54 +1000

    link: https://www.hackerrank.com/challenges/handshake/problem

---
+++ src/main/java/kadir/hackerrank/challange/Handshake.java
@@ -0,0 +1,15 @@
+package kadir.hackerrank.challange;
+
+public class Handshake {
+
+    /*
+     * Complete the 'handshake' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static int handshake(int n) {
+        return n*(n-1)/2;
+    }
+}
